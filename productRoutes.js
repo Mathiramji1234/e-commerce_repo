024d050d2177a597db352befde2cec9be405c543
Mathiramji1234 <mathiramji2006@gmail.com>
2025-12-17 10:00:17 +0530

    Add files via upload

---
+++ productRoutes.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const Product = require("../models/Product");
+
+const router = express.Router();
+
+// GET all products
+router.get("/", async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// GET single product by ID
+router.get("/:id", async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    res.json(product);
+  } catch (err) {
+    res.status(404).json({ message: "Product not found" });
+  }
+});
+
+module.exports = router;
